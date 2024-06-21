252ac156802f81dd7bcd71e7a6b4aacf5be8ae69
haseena-hassan <haseena2199@gmail.com>
2024-06-21 23:57:00 +0530

    Day-2 : Strategy Design Pattern

---
+++ Design Patterns/1-Strategy/WithoutStrategy/Vehicle.java
@@ -0,0 +1,7 @@
+/*
+ * Strategy Design Pattern
+ * 
+ *  -   When special capabilities across subclasses are duplicated or common with some children
+ *  -   Prevent code duplication by creating interfaces and implementing interfaces specially
+ * 
+ */
